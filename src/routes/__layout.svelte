1c4f7bf6bbff0a4f58a42b611eae118634abf246
Laszlo Juhasz <juhaszl@gmail.com>
2021-11-21 00:53:28 +0100

    add WindiCSS

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "virtual:windi.css"
+
+  // if you want to enable windi devtools
+  import { browser } from "$app/env";
+  if (browser) import("virtual:windi-devtools")
+  // ...
+</script>
+
+<main class="h-screen bg-red-900">
+	<slot />
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
+import WindiCSS from 'vite-plugin-windicss';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      plugins: [WindiCSS()],
+    },
+  },
 };
 
 export default config;
